92cbc2a67591b425a4bedfb1e5e073fe726ba93d
Joreinhard Munandar <jorensaragih@gmail.com>
2024-06-12 13:16:17 +1000

    fifth commit

--- public/js/header.js
+++ public/js/header.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
                             <li class="text-base text-secondcolor font-semibold"><a href="../index.html">Home</li>
                             <li class="text-base text-secondcolor font-semibold">Listing</li>
                             <li class="text-base text-secondcolor font-semibold">Company</li>
-                            <li class="text-base text-secondcolor font-semibold"><a href="/blog.html">Blog</a></li>
-                            <li class="text-base text-secondcolor font-semibold"><a href="/about.html">About Us</a></li>
+                            <li class="text-base text-secondcolor font-semibold"><a href="blog.html">Blog</a></li>
+                            <li class="text-base text-secondcolor font-semibold"><a href="about.html">About Us</a></li>
                         </ul>
                     </div>
                     <button class="hidden lg:flex w-40 bg-maincolor px-3 py-4 font-semibold rounded-xl text-sm justify-center border">Contact Us</button> 
@@ -28,8 +28,8 @@ document.addEventListener('DOMContentLoaded', () => {
                         <li><a href="../index.html" class="block text-sm font-semibold">Home</a></li>
                         <li><a href="#" class="block text-sm font-semibold">Listing</a></li>
                         <li><a href="#" class="block text-sm font-semibold">Company</a></li>
-                        <li><a href="/blog.html" class="block text-sm font-semibold">Blog</a></li>
-                        <li><a href="/about.html" class="block text-sm font-semibold">About Us</a></li>
+                        <li><a href="blog.html" class="block text-sm font-semibold">Blog</a></li>
+                        <li><a href="about.html" class="block text-sm font-semibold">About Us</a></li>
                     </ul>
                 </div>
             </div>
